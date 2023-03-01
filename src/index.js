8a860ebdc22b4a78a7896fae71150fc87f1d625b
Prince E <109649366+peeebamsc@users.noreply.github.com>
2023-03-01 09:58:21 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <div className="made-by">Made by Prince E. Eyo</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
